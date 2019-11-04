f209a8c3e8fb3fb38b018f235bdd3938ba0fe1ef
AdamMomen <adammuman81@gmail.com>
2019-11-04 21:16:20 +0100

    Build v1.6

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 $('#button').click(function () {
 	var value = $('#text').val() ; 
-	console.log(value)
 	if (value === '') {
 		return alert('Please enter a task !')
 	} else {
 	 $('#text').val('');	
 	return	$('#list').append('<div id="DIV"><li>' + value + '</li></div>');
 }
+});
+//This is for the enter key press
+$('#text').keypress(function(event){
+            var keycode = (event.keyCode ? event.keyCode : event.which);
+            if(keycode == '13'){
+                 var value = $('#text').val() ; 
+	if (value === '') {
+		return alert('Please enter a task !')
+	} else {
+	 $('#text').val('');	
+	return	$('#list').append('<div id="DIV"><li>' + value + '</li></div>');
+		}
+	}
+ event.stopPropagation();
 });
